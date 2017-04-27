94048cf3759c3843c629d3791ca2a2cfd018d368
npappassl <npappas@scottlogic.com>
2017-04-27 17:56:18 +0100

    first commit not bad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,38 @@
+import { OnInit } from "@angular/OnInit";
 import { Component } from '@angular/core';
+import { BookService } from 'app/book.service';
+import { Book } from "./book";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  providers: [BookService],
+  template:
+    `
+        <h1>Hello {{name}}</h1>
+        <ul>
+            <li *ngFor="let book of books; let i = index">
+              {{ book.Title }} {{ book.Id }} {{i}}
+            </li>
+        </ul>
+        <booksList name="{{name}}" booksS="{{books}}"></booksList>
+    `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  implements OnInit {
+    books: Book[] = [new Book()];
+    ngOnInit(): void {
+        this.getBooks();
+    }
+    getBooks(): void {
+      this.bookService.getBooks()
+        .then(books => {
+            this.books.length = 0;
+            for(let i in books){
+                console.log("books",i,books[i]);
+                this.books.push(books[i]);
+            }
+      });
+    }
+    name = 'Angular';
+    constructor(private bookService: BookService) { }
+
+}
